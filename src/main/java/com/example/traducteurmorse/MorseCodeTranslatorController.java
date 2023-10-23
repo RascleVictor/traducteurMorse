d9f36b0d1d6ae0752b7693800c0cc09d48ee225b
victor <victor.rascle@lyceeastier.com>
2023-10-23 15:57:24 +0200

    changement

--- src/main/java/com/example/traducteurmorse/MorseCodeTranslatorController.java
+++ src/main/java/com/example/traducteurmorse/MorseCodeTranslatorController.java
@@ -1,8 +1,13 @@
 package com.example.traducteurmorse;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 
 public class MorseCodeTranslatorController {
@@ -13,10 +18,57 @@ public class MorseCodeTranslatorController {
     private Label outputLabel;
 
     @FXML
+    private Circle led;
+
+    private int dotDuration = 500; // Durée d'un point en ms
+    private int dashDuration = 3 * dotDuration; // Durée d'un tiret en ms
+    private int spaceDuration = 7 * dotDuration; // Durée de l'espace entre les mots en ms
+    private int slashDuration = 3 * dotDuration; // Durée d'un slash en ms
+    private Timeline blinkingTimeline;
+
+    @FXML
+    private void initialize() {
+        blinkingTimeline = new Timeline(
+                new KeyFrame(Duration.ZERO, e -> led.setFill(javafx.scene.paint.Color.RED)),
+                new KeyFrame(Duration.millis(dotDuration), e -> led.setFill(javafx.scene.paint.Color.GREEN)),
+                new KeyFrame(Duration.millis(dotDuration * 2), e -> led.setFill(javafx.scene.paint.Color.RED)),
+                new KeyFrame(Duration.millis(dotDuration * 3), e -> led.setFill(javafx.scene.paint.Color.GREEN)),
+                new KeyFrame(Duration.millis(dotDuration * 4), e -> led.setFill(javafx.scene.paint.Color.RED)),
+                new KeyFrame(Duration.millis(dotDuration * 5), e -> led.setFill(javafx.scene.paint.Color.GREEN)),
+                new KeyFrame(Duration.millis(dotDuration * 6), e -> led.setFill(javafx.scene.paint.Color.RED)),
+                new KeyFrame(Duration.millis(dotDuration * 7), e -> led.setFill(javafx.scene.paint.Color.GREEN)),
+                new KeyFrame(Duration.millis(dotDuration * 8), e -> led.setFill(javafx.scene.paint.Color.RED))
+        );
+        blinkingTimeline.setCycleCount(1); // Un cycle unique
+    }
+
+
+    @FXML
     private void translateButtonAction() {
         String text = inputField.getText().toUpperCase();
         String translatedText = translateToMorse(text);
         outputLabel.setText("Morse Code: " + translatedText);
+
+        // Commencer l'animation de la LED lors de la traduction
+        playMorseAnimation(translatedText);
+    }
+
+    @FXML
+    private void playMorseAnimation(String morseText) {
+        blinkingTimeline.stop();
+
+        for (char c : morseText.toCharArray()) {
+            if (c == '.') {
+                blinkingTimeline.setCycleCount(1);
+            } else if (c == '-') {
+                blinkingTimeline.setCycleCount(3);
+            } else if (c == '/') {
+                blinkingTimeline.setCycleCount(3);
+            } else if (c == ' ') {
+                blinkingTimeline.setCycleCount(7);
+            }
+            blinkingTimeline.play();
+        }
     }
 
     private String translateToMorse(String text) {
@@ -42,7 +94,7 @@ public class MorseCodeTranslatorController {
                     }
                 }
                 if (index != -1) {
-                    morseText.append(morseCode[index]).append(" ");
+                    morseText.append(morseCode[index]).append(" / ");
                 }
             }
         }
